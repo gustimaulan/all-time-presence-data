a3215f84e73f3aadd4e861a2c2b9e6d0e7a8feb8
gustimaulanid <gmaulan47@gmail.com>
2026-01-19 14:50:49 +0700

    security: hide real backend URL from client and git

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL
-    ? `${import.meta.env.VITE_BACKEND_URL}/api`
-    : (import.meta.env.VITE_API_BASE_URL || '/api'),
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
